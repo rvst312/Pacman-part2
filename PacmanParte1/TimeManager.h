18a5a65ff3d8df9d36c30fd503ffe3ba71f0c2ba
CAMPUS-NET <CAMPUS-NET@CAMPUSNET>
2021-12-09 17:04:45 +0100

    Time.h y enemigos

---
+++ PacmanParte1/TimeManager.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <chrono>
+class TimeManager
+{
+	std::chrono::high_resolution_clock::time_point start
+		= std::chrono::high_resolution_clock::now();
+public:
+	unsigned int frameCount;
+	float time;
+	float deltaTime;
+
+	static TimeManager& getInstance();
+
+	void NextFrame();
+};
+
